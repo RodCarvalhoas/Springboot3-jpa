23a6dacc67bddc08e3622c3fc873b9aea1da93cb
Rodrigo Sutil <sutil_rodrigo@hotmail.com>
2022-12-06 20:31:32 -0300

    Project created

---
+++ src/main/java/com/rodrigo_sutil/course_Udemy/CourseUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo_sutil.course_Udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo_sutil/course_Udemy/CourseUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo_sutil.course_Udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
