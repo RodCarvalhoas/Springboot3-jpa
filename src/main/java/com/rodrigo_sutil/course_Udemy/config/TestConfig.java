c75b0e6642927f5361a84df527396ced5d1e6e06
Rodrigo Sutil <sutil_rodrigo@hotmail.com>
2022-12-08 21:56:13 -0300

    Service layer, component registration

--- src/main/java/com/rodrigo_sutil/course_Udemy/config/TestConfig.java
+++ src/main/java/com/rodrigo_sutil/course_Udemy/config/TestConfig.java
@@ -11,22 +11,19 @@ import com.rodrigo_sutil.course_Udemy.entities.User;
 import com.rodrigo_sutil.course_Udemy.repositories.UserRepository;
 
 @Configuration
-@Profile("test") 
-public class TestConfig implements CommandLineRunner{
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		User u1 = new User(null, "Rodrigo", "rodrigo@gmail.com", "81995511063", "123456");
 		User u2 = new User(null, "Rafael", "rafael@gmail.com", "81995102523", "123456");
-		
-		userRepository.saveAll(Arrays.asList(u1, u2));	}
-	
-	
-	
+
+		userRepository.saveAll(Arrays.asList(u1, u2));
+	}
+
 }
---
+++ src/main/java/com/rodrigo_sutil/course_Udemy/services/UserService.java
@@ -0,0 +1,28 @@
+package com.rodrigo_sutil.course_Udemy.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo_sutil.course_Udemy.entities.User;
+import com.rodrigo_sutil.course_Udemy.repositories.UserRepository;
+
+@Service
+public class UserService {	
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
