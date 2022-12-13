1b392218d6c7796e925967773a1c16270a660240
Rodrigo Sutil <sutil_rodrigo@hotmail.com>
2022-12-13 13:53:45 -0300

    Exception handling - findById

--- src/main/java/com/rodrigo_sutil/course_Udemy/services/UserService.java
+++ src/main/java/com/rodrigo_sutil/course_Udemy/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.rodrigo_sutil.course_Udemy.entities.User;
 import com.rodrigo_sutil.course_Udemy.repositories.UserRepository;
+import com.rodrigo_sutil.course_Udemy.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {	
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
